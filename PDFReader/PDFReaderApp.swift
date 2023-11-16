0fee9dc6f2342fabba9c339fcc7bede41a99712c
yusuf <yusuferkoc99@gmail.com>
2023-11-16 16:15:17 +0300

    Initial Commit

---
+++ PDFReader/PDFReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFReaderApp.swift
+//  PDFReader
+//
+//  Created by Yusuf Erkoç on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PDFReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
